ba46df5b2e3fdf70e6a0918c10639e8224f4bd87
YeongJin You <kyttyj12@naver.com>
2020-04-13 01:57:22 +0900

    front SingUp추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import SignUp from './SignUp';
+import SignIn from './SignIn';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SignUp />
+    <SignIn />
   </React.StrictMode>,
   document.getElementById('root')
 );
