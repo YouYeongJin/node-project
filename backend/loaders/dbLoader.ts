9fa0a18b915058077a1f663e3b6a30c64c7ad955
DESKTOP-QBKM203\YJHJ <kyttyj123@gmail.com>
2020-11-20 10:19:16 +0900

    초기에 디비연결확인 추가

--- backend/loaders/dbLoader.ts
+++ backend/loaders/dbLoader.ts
@@ -1,3 +1,4 @@
+import { exception } from "console";
 import mysql from "mysql";
 import mysql_config from "./config/db_config/mysql_config";
 import logger from "./config/log_config/logger";
@@ -5,7 +6,16 @@ import logger from "./config/log_config/logger";
 export default async ({ app }: { app: Express.Application }) => {
     global.__pool = await mysql.createPool(mysql_config);
 
-    global.__pool.on("acquire", (connection: mysql.Connection) => {
+    global.__pool.getConnection((err:Error, connection:mysql.PoolConnection) => {
+        if(err) {
+            logger.error(err.message);
+            throw err;
+        }
+        logger.info(`*디비 연결 확인*`);
+        connection.release();
+    });
+
+    global.__pool.on("acquire", (connection: mysql.PoolConnection) => {
         logger.info(`*커넥션 스레드ID : ${connection.threadId} 연결*`);
     });
 
@@ -13,7 +23,7 @@ export default async ({ app }: { app: Express.Application }) => {
         logger.info("*커넥션 대기 중*");
     });
 
-    global.__pool.on("release", (connection: mysql.Connection) => {
+    global.__pool.on("release", (connection: mysql.PoolConnection) => {
         logger.info(`*커넥션 스레드ID : ${connection.threadId} 반환*`);
     });
 };
